1c6d0965e833ae85db8356cbcbb15dc0c1745699
dawnwad <disv002@gmail.com>
2024-09-02 18:21:30 +0900

    0902
    App.js 파일 게시판 목록 만들기

---
+++ Source/SemiProject/src/main/reactproject/src/pages/Footer.js
@@ -0,0 +1,10 @@
+function Footer(){
+  return (
+    <footer>
+      <hr/>
+      푸터 입니다.
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Source/SemiProject/src/main/reactproject/src/pages/Header.js
@@ -0,0 +1,10 @@
+function Header(){
+  return (
+    <header>
+      헤더입니다.
+      <hr/>
+    </header>
+  );
+};
+
+export default Header;
