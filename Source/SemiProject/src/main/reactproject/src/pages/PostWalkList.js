350c7350843d739bb3b047e20e1e0be12b193d94
dawnwad <disv002@gmail.com>
2024-09-11 18:17:05 +0900

    0911
    PostWalkList 백엔드 구현, PostWalkDetail 백엔드 조금 구현

--- Source/SemiProject/src/main/reactproject/src/pages/PostWalkList.js
+++ Source/SemiProject/src/main/reactproject/src/pages/PostWalkList.js
@@ -1,115 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../assets/PostList.css';
-import axios from 'axios';
-// import CustomPagination from './CustomPagination';
 
-function WalkingTrail() {
-  axios.get('/walking/'+3+'/'+10)
-  .then(response => {
-    // setWalkingTrails(response.data);
-  })
-  .catch(error => {
-    console.error('Error fetching walkingTrail data: ', error);
-  });
-  
-}
-
-function WalkingTrail() {
-  axios.get('/walking/'+3+'/'+10)
-  .then(response => {
-    // setWalkingTrails(response.data);
-  })
-  .catch(error => {
-    console.error('Error fetching walkingTrail data: ', error);
-  });
-  
-}
-
-function PostWalkList({ isLoggedIn }) {
-
-  const [currentPage, setCurrentPage] = useState(1);    // 현재 페이지
+function WalkingTrailsList() {
+  const [walkingTrails, setWalkingTrails] = useState([]);
   const [postsPerPage, setPostsPerPage] = useState(10); // 한 페이지에 표시할 글 수
-  const [totalBlock, setTotalBlock] = useState('');
+  const [currentPage, setCurrentPage] = useState(1); // 1부터 시작하는 페이지 번호
+  const [totalPages, setTotalPages] = useState(); // 전체 페이지 수
+  const [searchInput, setSearchInput] = useState('');
   const [totalRecord, setTotalRecord] = useState('');
-  const [totalPages, setTotalPages] = useState(1);      // 전체 페이지수
-  const [walkingTrails, setWalkingTrails] = useState([]);   // 가져온 게시물 목록
-
-
-
-
-
-  // useEffect(() => {
-  //   axios.get(`/walking/list`, {
-  //     params: {
-  //       page: currentPage,        // 현재 페이지
-  //       numPerPage: postsPerPage  // 페이지당 게시물 수
-  //     }
-  //   })
-  //   .then((response) => {
-  //     setWalkingTrails(response.data.items);    // 서버에서 가져온 데이터 설정
-  //     setTotalPages(response.data.totalPages);  // 서버에서 반환된 총 페이지수 설정
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error fetching walking trail data: ', error);
-  //   });
-  // }, [currentPage, postsPerPage]);
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  }
-
-  // useEffect(() => {
-  //   axios.get('/api/your-endpoint')
-  //     .then(response => {
-  //       const totalPosts = response.data.totalPosts;  // 전체 게시글 수를 백엔드에서 받음
-  //       const pages = Math.ceil(totalPosts / postsPerPage);  // 페이지 수 계산
-  //       setTotalPages(pages);  // totalPages 값 설정
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching data:', error);
-  //     });
-  // }, [postsPerPage]);
+  const [totalBlock, setTotalBlock] = useState('');       // 전체 페이지 블록
+  const [currentBlock, setCurrentBlock] = useState(0);    // 현재 페이지 블록
+  const [searchCategory, setSearchCategory] = useState('null');
+  const [searchTerm, setSearchTerm] = useState('');
   
-
-
-
-
-
-
-  function SearchWalkingTrail(){
-    if(searchCategory == 'null'){
-      alert('카테고리를 선택하십시오')
-
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  
+  // 페이지 변경 처리
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber); // Spring Boot는 페이지 번호가 0부터 시작
+  };
+  
+  const handlePostsPerPageChange = (event) => {
+    setPostsPerPage(parseInt(event.target.value, 10));
+    if(currentPage === 1){
+      listCaller()
     }else{
-      axios.post('/walking/list',{
-        'page' : currentPage,
-        'numPerPage' : postsPerPage,
-        'keyField' : searchCategory,
-        'keyWord' : searchInput
-      })
-      .then(response => {
-        console.log(response.data)
-        setWalkingTrails(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching walkingTrail data: ', error);
-      });
+      setCurrentPage(1); // 한 페이지에 나타낼 글 수를 변경하면 첫 페이지로 이동
     }
+  };
+  
+  // const filteredPosts = walkingTrails.filter((post,i) => {
+  //   console.log("currentPage ", currentPage)
+  //   console.log("currentBlock ", currentBlock)
+  //   console.log("indexOfFirstPost ", indexOfFirstPost)
+  //   console.log("indexOfLastPost ", indexOfLastPost)
+  //   return i>=indexOfFirstPost && i<=indexOfLastPost;
+  // });
 
-  }
-  useEffect(() => {
-    // 백엔드로부터 게시글 데이터를 가져옴
-    // axios.get('/walking/'+3+'/'+10)
-    // .then(response => {
-    //   console.log(response.data)
-    //   setWalkingTrails(response.data);
-    // })
-    // .catch(error => {
-    //   console.error('Error fetching walkingTrail data: ', error);
-    // });
+  // const filteredPosts = walkingTrails.slice(indexOfFirstPost, indexOfLastPost);
 
-    
+  function listCaller(){
     axios.post('/walking/list',{
       'page' : currentPage,
       'numPerPage' : postsPerPage,
@@ -117,57 +50,21 @@ function PostWalkList({ isLoggedIn }) {
       'keyWord' : searchInput
     })
     .then(response => {
-      console.log(response.data)
+      console.log("currentPage ", currentPage);
+      console.log(response.data);
       setWalkingTrails(response.data.list);
       setTotalRecord(response.data.totalRecord);
+      setTotalPages(response.data.totalPages);
+      console.log(totalBlock);
       setTotalBlock(Math.ceil(response.data.totalRecord / postsPerPage));
     })
     .catch(error => {
       console.error('Error fetching walkingTrail data: ', error);
     });
-  }, [currentPage]);
-  
-  const [posts, setPosts] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchInput, setSearchInput] = useState('');
-  const [searchCategory, setSearchCategory] = useState('id');
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-
-  const filteredPosts = walkingTrails.filter((post,i) => {
-   
-    return i>=indexOfFirstPost && i<=indexOfLastPost;
-  });
-
-  //const currentPosts = walkingTrails.get .slice(indexOfFirstPost, indexOfLastPost);
-
-  // const handlePageChange = (pageNumber) => {
-  //   axios.get(`/walking/list?page=${pageNumber}&size=${postsPerPage}`)
-  //        .then(response => {
-  //         setWalkingTrails(response.data.content);    // 해당 페이지의 데이터를 설정
-  //         setCurrentPage(pageNumber);                 // 현재 페이지 업데이트
-  //        })
-  //        .catch(error => {
-  //         console.error('Error fetching walkingTrail data: ', error);
-  //        });
-  // };
-
-  const handleLike = (id) => {
-    if (isLoggedIn) {
-      setPosts(posts.map((post) =>
-        post.id === id
-          ? {
-              ...post,
-              likes: post.likedByUser ? post.likes - 1 : post.likes + 1,
-              likedByUser: !post.likedByUser,
-            }
-          : post
-      ));
-    } else {
-      alert('로그인 후 좋아요를 누를 수 있습니다.');
-    }
-  };
+  }
+  useEffect(() => {
+    listCaller()
+  }, [currentPage, postsPerPage, searchInput, searchCategory]);
 
   const handleSearch = () => {
     setSearchTerm(searchInput);
@@ -183,25 +80,26 @@ function PostWalkList({ isLoggedIn }) {
     setSearchCategory(event.target.value);
   };
 
-  const handlePostsPerPageChange = (event) => {
-    setPostsPerPage(parseInt(event.target.value, 10));
-    setCurrentPage(1); // 한 페이지에 나타낼 글 수를 변경하면 첫 페이지로 이동
-  };
-
-   //const totalPages = Math.ceil(총레코드 수 / postsPerPage);
-
+  function SearchWalkingTrail(){
+    if(searchCategory == 'null'){
+      alert('카테고리를 선택하십시오')
+    }else{
+      listCaller();
+    }
+  }
+  
   return (
     <div>
-      <h1>산책 경로 목록</h1>
+      <h1>Walking Trails</h1>
 
-      {/* 한 페이지에 표시할 글 수 선택하는 select 요소 */}
-      <div className="posts-per-page">
-        <select id="postsPerPageSelect" value={postsPerPage} onChange={handlePostsPerPageChange}>
-          <option value="10">10개씩 보기</option>
-          <option value="20">20개씩 보기</option>
-          <option value="50">50개씩 보기</option>
-        </select>
-      </div>
+       {/* 한 페이지에 표시할 글 수 선택하는 select 요소 */}
+       <div className="posts-per-page">
+          <select id="postsPerPageSelect" value={postsPerPage} onChange={handlePostsPerPageChange}>
+            <option value="10">10개씩 보기</option>
+            <option value="20">20개씩 보기</option>
+            <option value="50">50개씩 보기</option>
+          </select>
+        </div>
 
       <table>
         <thead>
@@ -215,7 +113,7 @@ function PostWalkList({ isLoggedIn }) {
           </tr>
         </thead>
         <tbody>
-          {filteredPosts.map((walkingTrails) => (
+          {walkingTrails.map((walkingTrails) => (
             <tr key={walkingTrails.wid}>
               <td>{walkingTrails.wid}</td>
               <td className='detail-td'>
@@ -225,7 +123,7 @@ function PostWalkList({ isLoggedIn }) {
               <td>{walkingTrails.coursLvNm}</td>
               <td>{walkingTrails.coursTmContent}</td>
               <td>
-                {isLoggedIn ? (
+                {/* {isLoggedIn ? (
                   <button onClick={() => handleLike(walkingTrails.id)}>
                     {walkingTrails.likedByUser ? '좋아요 취소' : '좋아요'} {walkingTrails.likes}
                   </button>
@@ -233,16 +131,26 @@ function PostWalkList({ isLoggedIn }) {
                   <button disabled>
                     좋아요 {walkingTrails.likes}
                   </button>
-                )}
+                )} */}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
 
+      {/* 페이지네이션 */}
       <div className="pagination">
-        {totalPages > 0 && 
-        Array.from({ length: totalBlock }, (_, i) => i + 1).map((pageNumber) => (
+      {currentBlock > 0 ? (
+        <button onClick={() => {
+          setCurrentPage((currentBlock - 1) * 10 + 1);
+          setCurrentBlock(currentBlock - 1);
+        }}>Prev...</button>
+      ) : null}
+
+      {/* 현재 블록에서 보여줄 페이지 버튼 생성 */}
+      {totalPages > 0 && 
+        Array.from({ length: Math.min(10, totalPages - currentBlock * 10) }, (_, i) => i + 1 + (currentBlock * 10))
+        .map((pageNumber) => (
           <button
             key={pageNumber}
             onClick={() => handlePageChange(pageNumber)}
@@ -250,17 +158,18 @@ function PostWalkList({ isLoggedIn }) {
           >
             {pageNumber}
           </button>
-        ))}
-      </div>
-      {/* <div>
-        {totalPages > 0 && (
-          <CustomPagination
-            pageCount={Math.max(1, totalPages - 1)}
-            onPageChange={handlePageChange}
-            currentPage={currentPage}
-          />
-        )}
-      </div> */}
+      ))}
+
+      {/* 다음 블록으로 이동하는 버튼 */}
+      {currentPage < totalPages ? (
+        <button onClick={() => {
+          setCurrentBlock(currentBlock + 1);
+          setCurrentPage(currentBlock * 10 + 1);
+        }}>...Next</button>
+      ) : null}
+    </div>
+
+
 
       <div className="search">
         <select value={searchCategory} onChange={handleCategoryChange} className="search-select">
@@ -282,4 +191,4 @@ function PostWalkList({ isLoggedIn }) {
   );
 }
 
-export default PostWalkList;
+export default WalkingTrailsList;
